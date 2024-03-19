986a7fce06adad883f834c20408ed0679d26f668
skbhicher <30347916+skbhicher@users.noreply.github.com>
2024-03-19 16:11:57 +0530

    Add files via upload

---
+++ jsMypractice.js
@@ -0,0 +1,11 @@
+
+let fullName="S K Choudhary";
+console.log(fullName);
+let x=null;
+console.log(x);
+let y=undefined;
+console.log(y);
+
+const fullName2="sharwan kumar"
+console.log(fullName2);
+
