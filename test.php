d493e76612033f60d82bed616ea22836edabbef7
skbhicher <30347916+skbhicher@users.noreply.github.com>
2024-07-08 14:08:27 +0530

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,15 @@
 <?php
 
-echo"PHP fiest page to display contents.";
+if(array_key_exists('button1', $_POST)) { 
+            button1(); 
+        } 
+        else if(array_key_exists('button2', $_POST)) { 
+            button2(); 
+        } 
+        function button1() { 
+            echo "This is Button1 that is selected"; 
+        } 
+        function button2() { 
+            echo "This is Button2 that is selected"; 
+        } 
 ?>
